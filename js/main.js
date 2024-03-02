9bf5cb37dbab5b0fb6e8c6f48a3fda6320c549f3
Mylena <mylenaleiteb@gmail.com>
2024-03-01 22:40:42 -0300

    icons e carrossel

--- js/main.js
+++ js/main.js
@@ -101,6 +101,26 @@
 		});
 	}
 
+	/*Carrossel*/
+	var radio = document.querySelector('.manual-btn')
+    var cont = 1
+
+    document.getElementById('radio1').checked = true
+
+    setInterval(() => {
+      proximaImg()
+    },3000)
+
+    function proximaImg() {
+      cont++
+
+      if(cont > 5){
+        cont = 1
+      }
+
+      document.getElementById('radio'+cont).checked = true
+    }
+
 	/*--/ Testimonials owl /--*/
 	$('#testimonial-mf').owlCarousel({
 		margin: 20,
